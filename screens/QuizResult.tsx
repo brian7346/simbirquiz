b2bb172b8b424e1f1cec3b3476962dd69e8881cd
a_pereladov <pereladov@hotmail.com>
2021-09-11 02:43:07 +0600

    quiz result

--- screens/QuizResult.tsx
+++ screens/QuizResult.tsx
@@ -1,28 +1,51 @@
-import * as React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import * as React from "react"
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native"
 
-import { RootStackScreenProps } from '../types';
-
-export default function QuizResult({ route }: RootStackScreenProps<'NotFound'>) {
-  console.log(route)
+import { RootStackScreenProps } from "../types"
+import Colors from "../constants/Colors";
+import map from 'lodash/map';
+import filter from "lodash/filter"
+export default function QuizResult({
+  route,
+}: RootStackScreenProps<"NotFound">) {
+  const getSuccess = (): boolean => {
+    map(route, (result: any) => {
+      console.log(result)
+    });
+    return true
+  };
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>This screen doesn't exist.</Text>
+      <View style={styles.resultContainer}>
+        {getSuccess() ? (
+          <Text style={styles.title}>Поздравляем! </Text>
+        ) : (
+          <Text style={styles.title}>
+            Кажется вы усвоили не все, давайте повторим!{" "}
+          </Text>
+        )}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  resultContainer: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    margin: 60,
+    marginHorizontal: 24,
+    borderRadius: 20,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
+    backgroundColor: Colors.dark.background,
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   link: {
     marginTop: 15,
@@ -30,6 +53,6 @@ const styles = StyleSheet.create({
   },
   linkText: {
     fontSize: 14,
-    color: '#2e78b7',
+    color: "#2e78b7",
   },
 });
