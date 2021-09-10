dca81d38fbb6432acf0ac01053eb64da9447b2f8
Max <MRoslov@alfabank.ru>
2021-09-11 02:05:22 +0400

    feat(*): start study button

--- screens/CardStackScreen.tsx
+++ screens/CardStackScreen.tsx
@@ -1,23 +1,28 @@
 import React from "react";
-import {
-  StyleSheet,
-  Text,
-  SafeAreaView,
-  Dimensions
-} from "react-native";
+import { StyleSheet, Text, SafeAreaView, Dimensions, View } from "react-native";
 import { UserAchivements } from "../components/UserAchivements";
 import { UserHeader } from "../components/UserHeader";
+import { UserStudy } from "../components/UserStudy";
 import Colors from "../constants/Colors";
 
-export default class CardStackScreen extends React.Component {
+export default class CardStackScreen extends React.Component<{
+  navigation: { navigate: (screen: string) => void };
+}> {
+  handleUserStudyClick = () => {
+    this.props.navigation.navigate("Quiz");
+  };
 
   render() {
     return (
-        <SafeAreaView style={styles.container}>
+      <SafeAreaView style={styles.container}>
+        <View style={{ flex: 2 }}>
           <UserHeader />
           <UserAchivements />
-          <Text>Cards </Text>
-        </SafeAreaView>
+        </View>
+        <View style={{ flex: 1, borderRadius: 10, overflow: "hidden" }}>
+          <UserStudy onClick={this.handleUserStudyClick} />
+        </View>
+      </SafeAreaView>
     );
   }
 }
@@ -26,5 +31,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: Colors.dark.background,
-  }
+  },
 });
